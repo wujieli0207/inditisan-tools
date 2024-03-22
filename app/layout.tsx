a59eb627435adb29ba0fba585b0b9835ef7a9538
wujieli0207 <wujieli0207@outlook.com>
2024-03-23 07:36:05 +0800

    chore: 增加百度统计配置和遗漏提交的文件

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
+import GoogleAnalytics from './GoogleAnalytics'
+import BaiDuAnalytics from './BaiDuAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,7 +34,18 @@ export default function RootLayout({
     <html lang="en">
       <Analytics />
       <SpeedInsights />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        {process.env.NODE_ENV === 'development' ? (
+          <></>
+        ) : (
+          <>
+            <BaiDuAnalytics />
+            <GoogleAnalytics />
+          </>
+        )}
+      </body>
     </html>
   )
 }
