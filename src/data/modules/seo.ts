565f6acc6a7143dfedb4867969efc1e0bb2a5bbf
wujieli0207 <wujieli0207@outlook.com>
2024-03-22 08:46:44 +0800

    feat: seo 链接更新

--- src/data/modules/seo.ts
+++ src/data/modules/seo.ts
@@ -12,6 +12,18 @@ const seo: ICatagory = {
       icon: 'https://icon.horse/icon/learningseo.io',
       description: '免费的 SEO 学习资源和工具',
     },
+    {
+      title: 'SEO Stuff',
+      url: 'https://www.seo-stuff.com/',
+      icon: 'https://icon.horse/icon/seo-stuff.com',
+      description: '找到合适的 SEO 关键词',
+    },
+    {
+      title: 'AITDK 扩展',
+      url: 'https://aitdk.com/zh-CN/extension/',
+      icon: 'https://icon.horse/icon/aitdk.com',
+      description: '流量 & Whois & 网站SEO分析的 chrome 插件',
+    },
   ],
 }
 
