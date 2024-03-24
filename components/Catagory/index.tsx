641435f31169d4acbf97fa523418239a76766b07
wujieli0207 <wujieli0207@outlook.com>
2024-03-25 07:01:31 +0800

    fix: 网站 icon 被压缩问题

--- components/Catagory/index.tsx
+++ components/Catagory/index.tsx
@@ -15,21 +15,28 @@ const Catagory: FC<IProps> = ({ catagory }) => {
       <div className="flex flex-col mb-12">
         <h2 className="text-2xl font-bold text-black">{title}</h2>
 
-        <div className="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {cardList.map((card) => {
             return (
-              <Link href={card.url} target="_blank" key={card.title}>
-                <div className="flex items-center p-4 bg-white rounded-md shadow-md w-full min-h-28 hover:cursor-pointer hover:shadow-lg">
-                  <div className="flex items-center justify-center w-10 h-10 border border-blue-100 rounded-full bg-blue-50">
-                    <Image
-                      width={18}
-                      height={18}
-                      src={card.icon}
-                      alt={card.title}
-                    />
+              <Link
+                href={card.url}
+                target="_blank"
+                key={card.title}
+                className="flex items-center h-28"
+              >
+                <div className="flex items-center p-4 w-full h-full bg-white rounded-md shadow-md  hover:cursor-pointer hover:shadow-lg">
+                  <div className="basis-1/5">
+                    <div className="flex items-center justify-center w-10 h-10 border border-blue-100 rounded-full bg-blue-50">
+                      <Image
+                        width={18}
+                        height={18}
+                        src={card.icon}
+                        alt={card.title}
+                      />
+                    </div>
                   </div>
 
-                  <div className="ml-4">
+                  <div className="basis-4/5">
                     <h2 className="font-semibold text-black">{card.title}</h2>
                     <p className="mt-2 text-sm text-gray-500">
                       {card.description}
