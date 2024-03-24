6dcde9acbe8a2f3bbea97129ea6cf9c314bb243f
wujieli0207 <wujieli0207@outlook.com>
2024-03-24 19:31:45 +0800

    refactor: svg icon 封装

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import Sidebar from '@/components/Sidebar'
 import data from '@/data'
 import { handleSearch } from '@/utils/search'
 import { useRef, useState } from 'react'
+import SearchIcon from '@/components/Icon/Search'
+import EnterIcon from '@/components/Icon/Enter'
 
 export default function Home() {
   const [currentNav, setCurrentNav] = useState(
@@ -61,50 +63,19 @@ export default function Home() {
             className="max-w-xl px-8 mx-auto w-full mt-16"
           >
             <div className="relative">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
+              <SearchIcon className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" />
               <input
                 type="text"
                 placeholder="Search"
                 className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
                 onKeyDown={(e) => {
                   if (e.key === 'Enter') {
-                    console.log(
-                      'e.currentTarget.value: ',
-                      e.currentTarget.value
-                    )
                     setFilterData(handleSearch(data, e.currentTarget.value))
                   }
                 }}
               />
               {/* 回车 icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 right-3"
-                viewBox="0 0 1024 1024"
-                version="1.1"
-                p-id="4792"
-                width="200"
-                height="200"
-              >
-                <path
-                  d="M900 386.016v-192q0-16.992-11.488-28.512t-28-11.488-28.512 11.488-12 28.512v192q0 92-64.992 156.992T598.016 608H258.016l112.992-112q11.008-12 11.008-28.512t-11.488-28-28-11.488-28.512 12L136 618.016q-12 12-12 28.512t12 28.512l183.008 183.008q12 12 28.512 12t28.512-11.488 12-28-12-28.512l-114.016-114.016h336q124.992 0 213.504-88.512t88.512-213.504z"
-                  p-id="4793"
-                  fill="#bfbfbf"
-                ></path>
-              </svg>
+              <EnterIcon className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 right-3" />
             </div>
           </div>
 
