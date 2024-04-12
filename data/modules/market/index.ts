e27fbdd809b4b8b17e0e9285053c19cd4411a305
wujieli0207 <wujieli0207@outlook.com>
2024-04-12 15:19:00 +0800

    feat: 增加 1000UserGuide 地址

--- data/modules/market/index.ts
+++ data/modules/market/index.ts
@@ -1,12 +1,13 @@
 import { ICatagory } from '@/types/data'
 import seo from './seo'
+import propagate from './propagate'
 
 const market: ICatagory = {
   key: 'market',
   title: '营销和推广',
   cards: [],
   icon: 'IconDeviceDesktopAnalytics',
-  children: [seo],
+  children: [seo, propagate],
 }
 
 export default market
---
+++ data/modules/market/propagate.ts
@@ -0,0 +1,16 @@
+import { ICatagory } from '@/types/data'
+
+const propagate: ICatagory = {
+  key: 'propagate',
+  title: '宣传渠道',
+  cards: [
+    {
+      title: '1000UserGuide',
+      url: 'https://1000userguide.com/',
+      icon: 'https://icon.horse/icon/1000userguide.com',
+      description: '找到你的前1000个用户',
+    },
+  ],
+}
+
+export default propagate
