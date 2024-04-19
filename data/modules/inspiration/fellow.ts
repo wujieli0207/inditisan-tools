aced16c6027b325d014b66ceb848e6641952aff6
wujieli0207 <wujieli0207@outlook.com>
2024-04-19 12:03:38 +0800

    feat: 增加独立开发同路人模块

---
+++ data/modules/inspiration/fellow.ts
@@ -0,0 +1,16 @@
+import { ICatagory } from '@/types/data'
+
+const fellow: ICatagory = {
+  key: 'fellow',
+  title: '同路人',
+  cards: [
+    {
+      title: 'Cali',
+      url: 'https://cali.so/',
+      icon: 'https://icon.horse/icon/cali.so',
+      description: '佐玩创始人',
+    },
+  ],
+}
+
+export default fellow
--- data/modules/inspiration/index.tsx
+++ data/modules/inspiration/index.tsx
@@ -1,13 +1,14 @@
 import { ICatagory } from '@/types/data'
 import information from './information'
 import guider from './guider'
+import fellow from './fellow'
 
 const inspiration: ICatagory = {
   key: 'inspiration',
   title: '灵感',
   cards: [],
   icon: 'IconSun',
-  children: [information, guider],
+  children: [information, guider, fellow],
 }
 
 export default inspiration
