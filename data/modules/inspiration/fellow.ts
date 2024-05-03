2b11ff7d3045a06b4c0cece2fdbe287115a6a376
wujieli0207 <wujieli0207@outlook.com>
2024-05-03 13:11:31 +0800

    feat: 更新领路人和同路人链接

--- data/modules/inspiration/fellow.ts
+++ data/modules/inspiration/fellow.ts
@@ -16,6 +16,31 @@ const fellow: ICatagory = {
       icon: 'https://icon.horse/icon/blog.jimmylv.info',
       description: '想做产品，却误入程序世界爱上写作的摄影师',
     },
+    {
+      title: '张小吉',
+      url: 'https://www.bmms.me/',
+      icon: 'https://icon.horse/icon/bmms.me',
+      description: '独立创客 & 数字游民',
+    },
+    {
+      title: 'Jiang',
+      url: 'https://notes.dabing.one/',
+      icon: 'https://icon.horse/icon/notes.dabing.one',
+      description: '交互设计师，探索如何利用计算机帮助人们学习',
+    },
+    {
+      title: 'J 实验室',
+      url: 'https://weijunext.com/',
+      icon: 'https://icon.horse/icon/weijunext.com',
+      description: 'J 实验室 | 分享技术/产品/生活',
+    },
+    {
+      title: 'Dawei Ma',
+      url: 'https://www.bmpi.dev/',
+      icon: 'https://icon.horse/icon/bmpi.dev',
+      description:
+        '全栈工程师，终身学习（认知杠杆）* 全栈技术（时间杠杆）* 投资理财（财务杠杆） => 被动收入（人生杠杆）',
+    },
   ],
 }
 
--- data/modules/inspiration/guider.ts
+++ data/modules/inspiration/guider.ts
@@ -10,6 +10,12 @@ const guider: ICatagory = {
       icon: 'https://icon.horse/icon/chuan.us',
       description: '很多投资方面的见解，在产品领域也也深受启发',
     },
+    {
+      title: '阮一峰',
+      url: 'https://ruanyifeng.com/',
+      icon: 'https://icon.horse/icon/ruanyifeng.com',
+      description: '前端大佬，周刊几乎是每周必看',
+    },
   ],
 }
 
